78707078b20909997f1eb2d7e213c99a59938b61
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-09-27 13:30:54 -0500

    solucion pregunta 01

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,11 @@ datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
+import csv
+
+
+# reader como que guarda un objeto iterable del csv, 
+# cada iteracion me da una lista de la fila
 
 
 def pregunta_01():
@@ -14,3 +19,17 @@ def pregunta_01():
     214
 
     """
+    with open("files/input/data.csv", "r") as f:
+        reader = csv.reader(f, delimiter="\t") 
+        total = 0
+        for row in reader:
+            total += int(row[1])
+        
+        return total
+
+
+
+pregunta_01()
+
+
+
