3106eed53058b0a1350ac2d3176ffd35bdbcafab
Juan Andres <jugiraldore@unal.edu.co>
2025-09-30 14:34:28 -0500

    solucion punto 9

--- homework/pregunta_08.py
+++ homework/pregunta_08.py
@@ -1,29 +1,42 @@
 """
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en el archivo data.csv. En este laboratorio
-solo puede utilizar las funciones y librerias basicas de python. No puede
-utilizar pandas, numpy o scipy.
+Genere una lista de tuplas, donde el primer elemento de cada tupla
+contiene  el valor de la segunda columna; la segunda parte de la tupla
+es una lista con las letras (ordenadas y sin repetir letra) de la
+primera  columna que aparecen asociadas a dicho valor de la segunda
+columna.
+
+Rta/
+[(0, ['C']),
+    (1, ['B', 'E']),
+    (2, ['A', 'E']),
+    (3, ['A', 'B', 'D', 'E']),
+    (4, ['B', 'E']),
+    (5, ['B', 'C', 'D', 'E']),
+    (6, ['A', 'B', 'C', 'E']),
+    (7, ['A', 'C', 'D', 'E']),
+    (8, ['A', 'B', 'D', 'E']),
+    (9, ['A', 'B', 'C', 'E'])]
+
 """
 
-
+import csv
 def pregunta_08():
-    """
-    Genere una lista de tuplas, donde el primer elemento de cada tupla
-    contiene  el valor de la segunda columna; la segunda parte de la tupla
-    es una lista con las letras (ordenadas y sin repetir letra) de la
-    primera  columna que aparecen asociadas a dicho valor de la segunda
-    columna.
+    diccionario = {}
+    with open("files/input/data.csv", "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            clave = int(row[1])
+            valor = row[0]
 
-    Rta/
-    [(0, ['C']),
-     (1, ['B', 'E']),
-     (2, ['A', 'E']),
-     (3, ['A', 'B', 'D', 'E']),
-     (4, ['B', 'E']),
-     (5, ['B', 'C', 'D', 'E']),
-     (6, ['A', 'B', 'C', 'E']),
-     (7, ['A', 'C', 'D', 'E']),
-     (8, ['A', 'B', 'D', 'E']),
-     (9, ['A', 'B', 'C', 'E'])]
+            if clave in diccionario:
+                diccionario[clave].append(valor)
 
-    """
+            else:
+                diccionario[clave] = [valor]
+
+    resultado = [(clave, sorted(list(set(valor)))) for clave, valor in diccionario.items() ]
+    resultado.sort()
+    return resultado
+
+
+pregunta_08()
