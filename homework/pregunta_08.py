ca08cfb59a259eb0ae04200ba7db21bc725dc1e0
Juan Andres <jugiraldore@unal.edu.co>
2025-09-30 14:43:40 -0500

    solución punto 8 mejorado

--- homework/pregunta_08.py
+++ homework/pregunta_08.py
@@ -20,6 +20,7 @@ Rta/
 """
 
 import csv
+from collections import defaultdict
 def pregunta_08():
     diccionario = {}
     with open("files/input/data.csv", "r") as f:
@@ -37,6 +38,22 @@ def pregunta_08():
     resultado = [(clave, sorted(list(set(valor)))) for clave, valor in diccionario.items() ]
     resultado.sort()
     return resultado
+
+
+def pregunta_08_mejorada():
+    letras_por_valor = defaultdict(set)  #ahorra la logica de decir si existe se agrega, sino se crea
+    with open("files/input/data.csv", "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            letra = row[0]          # columna 1 (A, B, C, ...)
+            valor = int(row[1])     # columna 2 (0..9)
+            letras_por_valor[valor].add(letra)
+
+    resultado = [(valor, sorted(letras)) for valor, letras in sorted(letras_por_valor.items())]
+    print(resultado)
+
+pregunta_08_mejorada()
+
 
 
 pregunta_08()
