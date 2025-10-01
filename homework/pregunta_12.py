287debf47e40b9a3d7ffc15a62ad3452afe6d0af
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-10-01 15:40:58 -0500

    solucion punto 12

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -1,17 +1,30 @@
 """
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en el archivo data.csv. En este laboratorio
-solo puede utilizar las funciones y librerias basicas de python. No puede
-utilizar pandas, numpy o scipy.
+Genere un diccionario que contengan como clave la columna 1 y como valor
+la suma de los valores de la columna 5 sobre todo el archivo.
+
+Rta/
+{'A': 177, 'B': 187, 'C': 114, 'D': 136, 'E': 324}
+
+E	1	1999-02-28	b,g,f	jjj:12,bbb:3,ddd:9,ggg:8,hhh:2
 """
+
+import csv
+from collections import defaultdict, Counter
 
 
 def pregunta_12():
-    """
-    Genere un diccionario que contengan como clave la columna 1 y como valor
-    la suma de los valores de la columna 5 sobre todo el archivo.
+    diccionario_sumas = defaultdict(int)
 
-    Rta/
-    {'A': 177, 'B': 187, 'C': 114, 'D': 136, 'E': 324}
+    with open("files/input/data.csv", 'r') as f:
+        reader = csv.reader(f, delimiter="\t")
 
-    """
+        for letra, *_, col5 in reader:
+            suma = sum([int(x.split(':')[-1])  for x in col5.split(',')])
+            diccionario_sumas[letra] += suma
+
+    return dict(sorted(diccionario_sumas.items()))  
+
+print(pregunta_12())      
+
+
+
