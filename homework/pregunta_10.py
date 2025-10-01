808c7e60f41a484857e846ffb3bf59b1361535bb
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-10-01 15:14:01 -0500

    solucion punto 10

--- homework/pregunta_10.py
+++ homework/pregunta_10.py
@@ -1,22 +1,30 @@
 """
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en el archivo data.csv. En este laboratorio
-solo puede utilizar las funciones y librerias basicas de python. No puede
-utilizar pandas, numpy o scipy.
-"""
+Retorne una lista de tuplas contengan por cada tupla, la letra de la
+columna 1 y la cantidad de elementos de las columnas 4 y 5.
 
+Rta/
+[('E', 3, 5),
+    ('A', 3, 4),
+    ...
+    ('E', 2, 3),
+    ('E', 3, 3)]
+
+E	1	1999-02-28	b,g,f	jjj:12,bbb:3,ddd:9,ggg:8,hhh:2
+"""
+import csv
 
 def pregunta_10():
-    """
-    Retorne una lista de tuplas contengan por cada tupla, la letra de la
-    columna 1 y la cantidad de elementos de las columnas 4 y 5.
+    resultado = []
+    with open("files/input/data.csv", 'r') as f:
+        reader = csv.reader(f, delimiter="\t")
+        for letra, *_, datos1, datos2 in reader:
+            datos1 = len(datos1.split(','))
+            datos2 = len(datos2.split(','))
+            resultado.append((letra, datos1,datos2 ))
+        
+    return resultado
 
-    Rta/
-    [('E', 3, 5),
-     ('A', 3, 4),
-     ...
-     ('E', 2, 3),
-     ('E', 3, 3)]
+pregunta_10()
 
 
-    """
+
