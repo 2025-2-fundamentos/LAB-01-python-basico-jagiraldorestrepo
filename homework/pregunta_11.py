e959df4e3ddc0e8f2de34798f1f52cdb983dcdb4
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-10-01 15:31:04 -0500

    solucion punto 11

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -1,18 +1,25 @@
 """
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en el archivo data.csv. En este laboratorio
-solo puede utilizar las funciones y librerias basicas de python. No puede
-utilizar pandas, numpy o scipy.
-"""
+Retorne un diccionario que contengan la suma de la columna 2 para cada
+letra de la columna 4, ordenadas alfabeticamente.
 
+Rta/
+{'a': 122, 'b': 49, 'c': 91, 'd': 73, 'e': 86, 'f': 134, 'g': 35}
+
+E	1	1999-02-28	b,g,f	jjj:12,bbb:3,ddd:9,ggg:8,hhh:2
+"""
+import csv
+from collections import defaultdict
 
 def pregunta_11():
-    """
-    Retorne un diccionario que contengan la suma de la columna 2 para cada
-    letra de la columna 4, ordenadas alfabeticamente.
+    suma = defaultdict(int)
+    with open("files/input/data.csv", "r") as f:
+        reader = csv.reader(f, delimiter="\t")
 
-    Rta/
-    {'a': 122, 'b': 49, 'c': 91, 'd': 73, 'e': 86, 'f': 134, 'g': 35}
+        for _, numero, _,  letras, _ in reader:
+            numero = int(numero)
+            
+            for letra in letras.split(','):
+                suma[letra] += numero
 
-
-    """
+    return dict(sorted(suma.items()))
+print(pregunta_11())
